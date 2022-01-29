4d21ab923a5d8ef871c225d751c490c252c24400
LiangShuang <liangshuang@sutu.tech>
2022-01-29 12:09:46 +0800

    initialize

---
+++ ChiaInfoRetriever/Program.cs
@@ -0,0 +1,18 @@
+﻿using ChiaApi;
+
+// command: redir :8666 :8555
+
+var cfg = new ChiaApiConfig("../../../../private_full_node.crt", "../../../../private_full_node.key", "10.179.0.196", 8666);
+var client = new FullNodeApiClient(cfg);
+//var s = await client.GetBlockchainStateAsync();
+//var s = await client.GetBlocksAsync(640000, 640003, false);
+//var s = await client.GetBlockRecordByHeightAsync(0);
+//var s = await client.GetAllMemPoolItemsAsync();
+//var s = await client.GetCoinRecordByNameAsync("83cfec441ee3c7d152c54dcb1f5e700949c5842074ecaee388b85e64d3a2b85d");// tx id
+//var s = await client.GetCoinRecordsByPuzzleHashAsync("0xa0b99203ac3cf4563ec0854096e52b8e015b2c436502cea065a1b840e9915f7b");//equal to address: xch15zueyqav8n69v0kqs4qfdeft3cq4ktzrv5pvagr95xuyp6v3taase36wyh
+//var s = await client.GetCoinRecordsByParentIdsAsync(new[] { "0xa9e68a49f3038c38b28af15d43c71c23545fda1a87bbb6dc59af2d7e502100c3" });
+var s = await client.GetCoinRecordByNameAsync("41858081f34c9859d5bad8941c40fd53158f4dfcb8a27dcbcfc4521c1790287b");// tx id
+var p = await client.GetCoinRecordByNameAsync(s.CoinRecord.Coin.ParentCoinInfo);// tx id
+var s2 = await client.GetPuzzleAndSolutionAsync (s.CoinRecord.Coin.ParentCoinInfo, s.CoinRecord.ConfirmedBlockIndex);
+
+var d = s.Success;
---
+++ WalletServer/Controllers/WalletController.cs
@@ -0,0 +1,45 @@
+using ChiaApi;
+using ChiaApi.Models.Responses.FullNode;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WalletServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalletController : ControllerBase
+    {
+        private readonly ILogger<WalletController> logger;
+        private readonly FullNodeApiClient client;
+
+        public WalletController(ILogger<WalletController> logger)
+        {
+            this.logger = logger;
+            // command: redir :8666 :8555
+            var cfg = new ChiaApiConfig("../../../../private_full_node.crt", "../../../../private_full_node.key", "10.179.0.196", 8666);
+            this.client = new FullNodeApiClient(cfg);
+        }
+
+        public record GetRecordsRequest(string[] puzzleHashes, ulong? startHeight = null, ulong? endHeight = null, bool includeSpentCoins = false);
+        public record GetRecordsResponse(ulong peekHeight, CoinRecordInfo[] coins);
+        public record CoinRecordInfo(string puzzleHash, CoinRecord[] records);
+
+        [HttpGet]
+        public async Task<ActionResult> GetRecords(GetRecordsRequest request)
+        {
+            if (request.puzzleHashes.Length > 20) return BadRequest("Valid puzzle hash number per request is 20");
+
+            var bcstaResp = await this.client.GetBlockchainStateAsync();
+            if (bcstaResp == null || !bcstaResp.Success || bcstaResp.BlockchainState?.Peak == null) return StatusCode(503, "Cannot get blockchain status.");
+
+            var list = new List<CoinRecordInfo>();
+            foreach (var hash in request.puzzleHashes)
+            {
+                var recResp = await this.client.GetCoinRecordsByPuzzleHashAsync(hash, request.startHeight, request.endHeight, request.includeSpentCoins);
+                if (recResp == null || !recResp.Success || recResp.CoinRecords == null) return StatusCode(503, "Cannot get records.");
+                list.Add(new CoinRecordInfo(hash, recResp.CoinRecords.ToArray()));
+            }
+
+            return Ok(new GetRecordsResponse(bcstaResp.BlockchainState.Peak.Height, list.ToArray()));
+        }
+    }
+}
