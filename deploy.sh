5db6fbff11c195b96edaa2e4fd3b21b9f6b92051
LiangShuang <liangshuang@sutu.tech>
2022-07-19 22:09:39 +0800

    enhance script

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,25 @@ mkdir -p $BASEPATH/bin/syncer
 mkdir -p $BASEPATH/bin/api
 unzip -o NodeDBSyncer.zip -d $BASEPATH/bin/syncer
 unzip -o WalletServer.zip -d $BASEPATH/bin/api
-source ~/.bashrc
 
-start() {
-	NAME=$1
-	PATH=$2
-	BINARY=$3
-	daemon --name=$NAME --restart
-	if [ $? -ne 0 ]; then
-		nohup daemon --name="$NAME" --respawn -f --output=$BASEPATH/log/${NAME}.log dotnet $BINARY >/dev/null 2>&1 &
-		echo "$NAME started"
-	else
-		echo "$NAME restarted"
-	fi
+function start {
+    NAME=$1
+    WORKPATH=$2
+    BINARY=$3
+    PIDFILE=$BASEPATH/pid/${NAME}
+    cd $WORKPATH
+    source $BASEPATH/config/$NAME
+    cat $BASEPATH/config/$NAME
+    daemon --name=$NAME --running
+    if [ $? -ne 0 ]; then
+		echo "$NAME starting"
+    else
+		echo "$NAME restarting"
+		pid=$(head -n 1 $PIDFILE)
+		daemon --name=$NAME --stop
+		wait $pid
+    fi
+	nohup daemon --name="$NAME" --respawn -f --pidfile=$PIDFILE --output=$BASEPATH/log/${NAME}.log dotnet $BINARY >/dev/null 2>&1 &
 }
 
 start pawket-api $BASEPATH/bin/api WalletServer.dll
