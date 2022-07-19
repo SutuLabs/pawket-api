8683a96feede9024e161b3c59ae0a31b6f8f9269
LiangShuang <liangshuang@sutu.tech>
2022-07-19 11:02:43 +0800

    separate helper extensions

---
+++ WalletServer/Helpers/HexHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace WalletServer.Helpers
+{
+    public static class HexHelper
+    {
+        public static string Unprefix0x(this string hex)
+        {
+            return hex.StartsWith("0x") ? hex[2..] : hex;
+        }
+
+        public static string Prefix0x(this string hex)
+        {
+            return hex.StartsWith("0x") ? hex : $"0x{hex}";
+        }
+
+        public static string ToHexWithPrefix0x(this byte[]? buff)
+        {
+            var hex = HexMate.Convert.ToHexString(buff ?? Array.Empty<byte>(), HexMate.HexFormattingOptions.Lowercase);
+            return $"0x{hex}";
+        }
+
+        public static byte[] ToHexBytes(this string hex)
+        {
+            return HexMate.Convert.FromHexString(hex.Unprefix0x().AsSpan());
+        }
+    }
+}
--- WalletServer/Helpers/Misc.cs
+++ WalletServer/Helpers/Misc.cs
@@ -2,11 +2,6 @@
 {
     public static class Misc
     {
-        public static string Unprefix0x(this string hex)
-        {
-            return hex.StartsWith("0x") ? hex[2..] : hex;
-        }
-
         public static string GetRealIp(this HttpContext httpContext)
         {
             if (httpContext.Request.Headers.TryGetValue("X-Real-IP", out var realIp) && !string.IsNullOrWhiteSpace(realIp))
@@ -15,17 +10,6 @@
             }
 
             return httpContext.Connection.RemoteIpAddress?.ToString() ?? "";
-        }
-
-        public static string Prefix0x(this string hex)
-        {
-            return hex.StartsWith("0x") ? hex : $"0x{hex}";
-        }
-
-        public static string ToHexWithPrefix0x(this byte[]? buff)
-        {
-            var hex = HexMate.Convert.ToHexString(buff ?? Array.Empty<byte>(), HexMate.HexFormattingOptions.Lowercase);
-            return $"0x{hex}";
         }
 
     }
