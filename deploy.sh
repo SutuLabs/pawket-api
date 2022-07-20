e020b4c63b1fac0d5e9d50d3dd977c9cebd28b1f
LiangShuang <liangshuang@sutu.tech>
2022-07-20 12:17:49 +0800

    enhance deploy script

--- deploy.sh
+++ deploy.sh
@@ -15,17 +15,24 @@ function start {
     PIDFILE=$BASEPATH/pid/${NAME}
     cd $WORKPATH
     source $BASEPATH/config/$NAME
-    cat $BASEPATH/config/$NAME
-    daemon --name=$NAME --running
+    daemon --name=$NAME --pidfile=$PIDFILE --running
     if [ $? -ne 0 ]; then
 		echo "$NAME starting"
     else
 		echo "$NAME restarting"
 		pid=$(head -n 1 $PIDFILE)
-		daemon --name=$NAME --stop
-		wait $pid
+		daemon --name=$NAME --pidfile=$PIDFILE --stop
+		echo -n "waiting previous process[$pid] exit"
+		while [ -e /proc/$pid ]
+		do
+			echo -n .
+			sleep .5
+		done
+		echo
     fi
+
 	nohup daemon --name="$NAME" --respawn -f --pidfile=$PIDFILE --output=$BASEPATH/log/${NAME}.log dotnet $BINARY >/dev/null 2>&1 &
+	echo "$NAME started"
 }
 
 start pawket-api $BASEPATH/bin/api WalletServer.dll
