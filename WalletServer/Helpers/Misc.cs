238777566341fbb557870a72ba6c6f02400f8714
LiangShuang <liangshuang@sutu.tech>
2022-08-30 12:03:12 +0800

    simplify file

--- WalletServer/Helpers/Misc.cs
+++ WalletServer/Helpers/Misc.cs
@@ -1,23 +1,22 @@
-﻿namespace WalletServer.Helpers
-{
-    public static class Misc
-    {
-        public static string GetRealIp(this HttpContext httpContext)
-        {
-            if (httpContext.Request.Headers.TryGetValue("X-Real-IP", out var realIp) && !string.IsNullOrWhiteSpace(realIp))
-            {
-                return realIp;
-            }
+﻿namespace WalletServer.Helpers;
 
-            return httpContext.Connection.RemoteIpAddress?.ToString() ?? "";
+public static class Misc
+{
+    public static string GetRealIp(this HttpContext httpContext)
+    {
+        if (httpContext.Request.Headers.TryGetValue("X-Real-IP", out var realIp) && !string.IsNullOrWhiteSpace(realIp))
+        {
+            return realIp;
         }
 
-        public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) where T : class
+        return httpContext.Connection.RemoteIpAddress?.ToString() ?? "";
+    }
+
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) where T : class
+    {
+        foreach (var item in source)
         {
-            foreach (var item in source)
-            {
-                if (item is not null) yield return item;
-            }
+            if (item is not null) yield return item;
         }
     }
 }
